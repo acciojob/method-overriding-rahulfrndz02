44bc03f93d87c0978864495e1474eb545936f9ba
Rahul Raj <rahulfrndz02@gmail.com>
2022-12-10 22:09:12 +0530

    My Commit

--- src/main/java/com/driver/Main.java
+++ src/main/java/com/driver/Main.java
@@ -1,21 +1,19 @@
 package com.driver;
 
 public class Main {
-    public static class A {
+    public static void main(String[] args) {
+        A obj = new B();
+        obj.meth();
+    }
+}
+    class A {
         public String meth() {
             return "Invoking method from class A";
         }
     }
-    public static class B extends A{
-
-
+    class B extends A{
         @Override
         public String meth() {
             return "Method is overridden in Extended class B";
         }
     }
-    public static void main(String[] args){
-        B obj = new B();
-        obj.meth();
-    }
-}
